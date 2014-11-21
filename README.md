82a17510cc5fe7bde566eefebd24ced882c5c4ff
stuart-warren <stuartwarren@ntlworld.com>
2014-11-21 00:57:56 +0000

    initial hack

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 elgrep
 ======
 
-CLI tool for Elasticsearch - in progress
+CLI tool for Elasticsearch Logstash indexes - in progress
+
+Usage:
+-----
+```
+$ elgrep -h
+Usage of elgrep:
+  -f=[@timestamp message]: fields to return (comma separated)
+  -j=false: results in json
+  -m=500: max number of results
+  -prefix="logstash-": index name prefix (before date)
+  args: query string query
+```
+Queries server at env ELASTICSEARCH_URL or http://localhost:9200 by default
+
+Currently only queries the index with todays date.
+
+See: http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#query-string-syntax for details on query string syntax
+
---
+++ elgrep.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/olivere/elastic"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	DEFAULT_ELASTICSEARCH_HOST = "http://localhost:9200"
+	DEFAULT_INDEX_PREFIX       = "logstash-"
+	DEFAULT_DATE_FORMAT        = "2006.01.02"
+	DEFAULT_NUM_RESULTS        = 500
+)
+
+type fieldName []string
+
+var (
+	ec             *elastic.Client
+	numResults     *int
+	indexPrefix    *string
+	stringQuery    = "*"
+	jsonFlag       *bool
+	fieldSeparator           = " "
+	fieldNameFlag  fieldName = []string{"@timestamp", "message"}
+)
+
+func init() {
+	var err error
+	var host string
+	host = os.Getenv("ELASTICSEARCH_HOST")
+	if host == "" {
+		host = DEFAULT_ELASTICSEARCH_HOST
+	}
+	ec, err = elastic.NewClient(http.DefaultClient, host)
+	if err != nil {
+		log.Fatal("can't connect to elasticsearch", host, err)
+	}
+}
+
+func (s *fieldName) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *fieldName) Set(value string) error {
+	if len(*s) > 0 {
+		*s = []string{}
+	}
+	for _, f := range strings.Split(value, ",") {
+		*s = append(*s, f)
+	}
+	return nil
+}
+
+func config() {
+	numResults = flag.Int("m", DEFAULT_NUM_RESULTS, "max number of results")
+	indexPrefix = flag.String("prefix", DEFAULT_INDEX_PREFIX, "index name prefix (before date)")
+	jsonFlag = flag.Bool("j", false, "results in json")
+	flag.Var(&fieldNameFlag, "f", "fields to return (comma separated)")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) > 0 {
+		stringQuery = strings.Join(args, " ")
+	}
+}
+
+func main() {
+	config()
+	duration, _ := time.ParseDuration("-0h")
+	indexDate := time.Now().Add(duration).UTC().Format(DEFAULT_DATE_FORMAT)
+	index := *indexPrefix + indexDate
+	// fmt.Println(index)
+	// fmt.Println("Query - ",stringQuery)
+	// fmt.Println(*numResults)
+	// fmt.Println(*numResults)
+	// fmt.Println(fieldNameFlag)
+
+	q := elastic.NewQueryStringQuery(stringQuery)
+	res, err := ec.Search().
+		Index(index).
+		Query(q).
+		From(0).Size(*numResults).
+		Sort("@timestamp", true).
+		Pretty(true).
+		Do()
+
+	if err != nil {
+		log.Fatal("issue with query", err)
+	}
+
+	if res.Hits != nil {
+		fmt.Printf("Found a total of %d messages\n", res.Hits.TotalHits)
+		for _, hit := range res.Hits.Hits {
+			// hit.Index contains the name of the index
+
+			if *jsonFlag {
+				fmt.Printf("%s\n", *hit.Source)
+			} else {
+				// Deserialize hit.Source into a message (could also be just a map[string]interface{}).
+				var m map[string]interface{}
+				err := json.Unmarshal(*hit.Source, &m)
+				if err != nil {
+					log.Fatal("Bad json")
+				}
+
+				var fields []string
+				for _, name := range fieldNameFlag {
+					fields = append(fields, fmt.Sprintf("%v", m[name]))
+				}
+				fmt.Printf("%s\n", strings.Join(fields, fieldSeparator))
+			}
+		}
+	}
+}
